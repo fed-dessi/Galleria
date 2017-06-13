6839ab1fd9db17ffb7f833b4d5aa6ca8421eb0c1
Federico <Federico@DESKTOP-KF3KNHM.lan>
2017-06-13 03:05:33 +0200

    13/06
    Pulizia OO dei controller

--- galleria/src/main/java/it/uniroma3/galleria/controller/InserimentoController.java
+++ galleria/src/main/java/it/uniroma3/galleria/controller/InserimentoController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,12 +20,8 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import it.uniroma3.galleria.model.Autore;
 import it.uniroma3.galleria.model.Quadro;
-import it.uniroma3.galleria.model.RuoliUtente;
-import it.uniroma3.galleria.model.Utente;
 import it.uniroma3.galleria.service.AutoreService;
 import it.uniroma3.galleria.service.QuadroService;
-import it.uniroma3.galleria.service.RuoliUtenteService;
-import it.uniroma3.galleria.service.UtenteService;
 
 @Controller
 @SessionAttributes("quadro")
@@ -36,10 +31,6 @@ public class InserimentoController {
 	QuadroService service;
 	@Autowired
 	AutoreService aService;
-	@Autowired
-	UtenteService uService;
-	@Autowired
-	RuoliUtenteService ruService;
 	
 	@GetMapping(value = "/inserimento")
 	public String inserimentoPagina(Quadro quadro, Model model){
@@ -101,40 +92,5 @@ public class InserimentoController {
 		model.addAttribute(quadro);
 		model.addAttribute("inseritoCorrettamente", true);
 		return "/inserimento/inserimentoAutore";
-	}
-	
-	@GetMapping(value = "/register")
-	public String register(Utente utente, Model model){
-		return "register";
-	}
-	
-	@PostMapping(value = "/register")
-	public String registraUtente(@Valid @ModelAttribute Utente utente, BindingResult bindingResult, HttpServletRequest request, @RequestParam(value = "ConfermaPassword", required = false) String confermaPassword,Model model){
-		
-		if(bindingResult.hasErrors()){
-		    List<FieldError> errors = bindingResult.getFieldErrors();
-		    for (FieldError error : errors ) {
-		        System.out.println (error.getObjectName() + " - " + error.getDefaultMessage());
-		    }
-			return "/register";
-		}
-		if(uService.getUtenteByUsername(utente.getUsername()) != null){
-			if(uService.getUtenteByUsername(utente.getUsername()).getUsername().equals(utente.getUsername())){
-				model.addAttribute("usernameEsistente", true);
-				model.addAttribute("confermaPassword", confermaPassword);
-				return "/register";
-			}
-		}else{
-			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-			String password = passwordEncoder.encode(utente.getPassword());
-			utente.setPassword(password);
-			utente.setEnabled(true);
-			RuoliUtente ru = new RuoliUtente("USER", utente);
-			ruService.inserisciRuoliUtente(ru);
-			model.addAttribute(utente);
-			model.addAttribute("utenteInserito", true);
-			return "/register";
-		}
-		return "/register";
 	}
 }
---
+++ galleria/src/main/java/it/uniroma3/galleria/controller/RegistrazioneController.java
@@ -0,0 +1,67 @@
+package it.uniroma3.galleria.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.uniroma3.galleria.model.RuoliUtente;
+import it.uniroma3.galleria.model.Utente;
+import it.uniroma3.galleria.service.RuoliUtenteService;
+import it.uniroma3.galleria.service.UtenteService;
+
+@Controller
+public class RegistrazioneController {
+
+
+	@Autowired
+	UtenteService uService;
+	@Autowired
+	RuoliUtenteService ruService;
+	
+	@GetMapping(value = "/register")
+	public String register(Utente utente, Model model){
+		return "register";
+	}
+	
+	@PostMapping(value = "/register")
+	public String registraUtente(@Valid @ModelAttribute Utente utente, BindingResult bindingResult, HttpServletRequest request, @RequestParam(value = "ConfermaPassword", required = false) String confermaPassword,Model model){
+		
+		if(bindingResult.hasErrors()){
+		    List<FieldError> errors = bindingResult.getFieldErrors();
+		    for (FieldError error : errors ) {
+		        System.out.println (error.getObjectName() + " - " + error.getDefaultMessage());
+		    }
+			return "/register";
+		}
+		if(uService.getUtenteByUsername(utente.getUsername()) != null){
+			if(uService.getUtenteByUsername(utente.getUsername()).getUsername().equals(utente.getUsername())){
+				model.addAttribute("usernameEsistente", true);
+				model.addAttribute("confermaPassword", confermaPassword);
+				return "/register";
+			}
+		}else{
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			String password = passwordEncoder.encode(utente.getPassword());
+			utente.setPassword(password);
+			utente.setEnabled(true);
+			RuoliUtente ru = new RuoliUtente("USER", utente);
+			ruService.inserisciRuoliUtente(ru);
+			model.addAttribute(utente);
+			model.addAttribute("utenteInserito", true);
+			return "/register";
+		}
+		return "/register";
+	}
+}
